8ec9011f7ef6faa5731d1b1976acba22e64da341
maldevel <maldevel@mail.com>
2017-11-10 18:09:06 +0200

    banners script added

---
+++ hardening/banners.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#    This file is part of blue-team
+#    Copyright (C) 2017 @maldevel
+#    https://github.com/maldevel/blue-team
+#    
+#    blue-team - Blue Team Scripts.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   
+#    For more see the file 'LICENSE' for copying permission.
+
+
+
+chown root:root /etc/motd
+chmod 644 /etc/motd
+echo "Authorized uses only. All activity may be monitored and reported." > /etc/motd
+  
+chown root:root /etc/issue
+chmod 644 /etc/issue
+echo "Authorized uses only. All activity may be monitored and reported." > /etc/issue
+  
+chown root:root /etc/issue.net
+chmod 644 /etc/issue.net
+echo "Authorized uses only. All activity may be monitored and reported." > /etc/issue.net
+
