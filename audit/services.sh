af7fcb9d4ead7cde2656fbef77c8cff9a6559811
maldevel <maldevel@mail.com>
2017-11-20 20:35:49 +0200

    services audit script updated

--- audit/services.sh
+++ audit/services.sh
@@ -24,21 +24,38 @@
 
 
 echo
-echo -e "\e[97m>> services audit in progress <<"
-echo
+echo -e "\e[1;95m-------------------------[services audit in progress]-------------------------"
 
-echo -e "\e[96m>> Checking if avahi-daemon service is enabled..\e[39m"
-systemctl is-enabled avahi-daemon
 
-echo 
+service=$(systemctl is-active avahi-daemon >/dev/null 2>&1 && echo 1 || echo 0)
+if [ $service -eq 1 ];
+then
+  status="\e[91m[ BAD ]"
+  #exit
+else
+  status="\e[92m[ GOOD ]"
+fi
+echo -e "\e[39m[*] Checking if avahi-daemon service is disabled\t\t\t\t\t$status"
 
-echo -e "\e[96m>> Checking if cups service is enabled..\e[39m"
-systemctl is-enabled cups
+service=$(systemctl is-active cups >/dev/null 2>&1 && echo 1 || echo 0)
+if [ $service -eq 1 ];
+then
+  status="\e[91m[ BAD ]"
+  #exit
+else
+  status="\e[92m[ GOOD ]"
+fi
+echo -e "\e[39m[*] Checking if cups service is disabled\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if rpcbind service is enabled..\e[39m"
-systemctl is-enabled rpcbind
+service=$(systemctl is-active rpcbind >/dev/null 2>&1 && echo 1 || echo 0)
+if [ $service -eq 1 ];
+then
+  status="\e[91m[ BAD ]"
+  #exit
+else
+  status="\e[92m[ GOOD ]"
+fi
+echo -e "\e[39m[*] Checking if rpcbind service is disabled\t\t\t\t\t\t$status"
 
 echo -e "\e[39m"
 
