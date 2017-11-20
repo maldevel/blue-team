558efc0f63e02c9ca875c6c90a5b69852a32a288
maldevel <maldevel@mail.com>
2017-11-20 19:44:49 +0200

    output updated for users-groups

--- audit/users-groups.sh
+++ audit/users-groups.sh
@@ -24,183 +24,156 @@
 
 
 echo
-echo -e "\e[97m>> users and groups audit in progress <<"
-echo
+echo -e "\e[1;95m-------------------------[users and groups audit in progress]-------------------------"
 
-echo -e "\e[96m>> Checking Maximum number of days of password usage.."
 signature=$(grep -cP '^PASS_MAX_DAYS\s+90$' /etc/login.defs)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mMaximum number of days has not been set to 90..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mMaximum number of days has been set to 90."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking Maximum number of days of password usage\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking Minimum number of days between password changes.."
 signature=$(grep -cP '^PASS_MIN_DAYS\s+5$' /etc/login.defs)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mMinimum number of days has not been set to 5..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mMinimum number of days has been set to 5."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking Minimum number of days between password changes\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking Number of days warning before password expiration.."
 signature=$(grep -cP '^PASS_WARN_AGE\s+10$' /etc/login.defs)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mMinimum number of days has not been set to 5..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mMinimum number of days has been set to 5."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking Number of days warning before password expiration\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking users locking after inactivity.."
 signature=$(useradd -D | grep -cP '^INACTIVE=30$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91musers locking after inactivity has not been set to 30..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92musers locking after inactivity has been set to 30."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking users locking after inactivity\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking root primary group.."
 signature=$(id -gn root| grep -cP '^root$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mroot primary group is not root..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mroot primary group is root."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking root primary group\t\t\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking libpam-cracklib installation.."
 installed=$(dpkg-query -W -f='${Status}' libpam-cracklib 2>/dev/null | grep -c "ok installed")
 if [ $installed -eq 0 ];
 then
-  echo -e "\e[91mlibpam-cracklib is not installed..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mlibpam-cracklib is already installed."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking libpam-cracklib installation\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking minimum password length.."
 signature=$(grep -cP '.*minlen=14.*' /etc/pam.d/common-password)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mminimum password length has not been set to 14..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mminimum password length has been set to 14."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking minimum password length\t\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if username in password is allowed.."
 signature=$(grep -cP '.*reject_username.*' /etc/pam.d/common-password)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91musername in password is allowed..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92musername in password is not allowed."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if username in password is allowed\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if Password complexity class.."
 signature=$(grep -cP '.*minclass=4.*' /etc/pam.d/common-password)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mPassword complexity class is not 4..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mPassword complexity class is 4."
+  status="\e[92m[ GOOD ]"
 fi
 
 signature=$(grep -cP '.*dcredit=-1 ucredit=-1 ocredit=-1 lcredit=-1.*' /etc/pam.d/common-password)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mPassword complexity is not configured..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mPassword complexity is configured."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Password complexity class\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if passwords which contain more than 2 same consecutive characters are rejected.."
 signature=$(grep -cP '.*maxrepeat=2.*' /etc/pam.d/common-password)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mpasswords which contain more than 2 same consecutive characters are not rejected..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mpasswords which contain more than 2 same consecutive characters are rejected."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if passwords with 2 same consecutive characters are rejected\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking last 24 passwords is enabled.."
 signature=$(grep -cP '.*remember=24.*' /etc/pam.d/common-password)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mlast 24 passwords is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mlast 24 passwords is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking last 24 passwords is enabled\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if accounts are getting locked out after unsuccessful consecutive login attempts.."
 signature=$(grep -cP '.*auth required pam_tally2\.so onerr=fail audit silent deny=5 unlock_time=1200.*' /etc/pam.d/login)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91maccounts are not getting locked out after 5 unsuccessful consecutive login attempts for 20 minutes..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92maccounts are getting locked out after 5 unsuccessful consecutive login attempts for 20 minutes."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if accounts locked out after unsuccessful login attempts\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if non-local logins to privileged accounts are not allowed.."
 signature=$(grep -cP '^-:wheel:ALL EXCEPT LOCAL.*' /etc/security/access.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mnon-local logins to privileged accounts are allowed..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mnon-local logins to privileged accounts are not allowed."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if non-local logins to privileged accounts are not allowed\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking delay time between login prompts.."
 signature=$(grep -cP '.*delay=10000000.*' /etc/pam.d/login)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mdelay time between login prompts has not been set to 10s..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mdelay time between login prompts has been set to 10s."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking delay time between login prompts\t\t\t\t\t\t$status"
 
 echo -e "\e[39m"
 
