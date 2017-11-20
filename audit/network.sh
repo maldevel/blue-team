bb4c18e0365cc1755822f5e8308206184147eff7
maldevel <maldevel@mail.com>
2017-11-20 21:06:26 +0200

    network and nginx scripts updated

--- audit/network.sh
+++ audit/network.sh
@@ -24,357 +24,357 @@
 
 
 echo
-echo -e "\e[97m>> network audit in progress <<"
-echo
+echo -e "\e[1;95m-------------------------[network audit in progress]-------------------------"
 
-echo -e "\e[96m>> Checking if IP forwarding is disabled.."
 signature=$(grep -cP '^net\.ipv4\.ip_forward=0$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mIP forwarding is not disabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mIP forwarding is disabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if IP forwarding is disabled\t\t\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.ip_forward| grep -cP '^net\.ipv4\.ip_forward\s=\s0$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mIP forwarding is not disabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mIP forwarding is disabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if IP forwarding is disabled for active kernel\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if packet redirect is disabled.."
 signature=$(grep -cP '^net\.ipv4\.conf\.all\.send_redirects\s=\s0$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mpacket redirect (all) is not disabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mpacket redirect (all) is disabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if packet redirect is disabled (all)\t\t\t\t\t$status"
 
 signature=$(grep -cP '^net\.ipv4\.conf\.default\.send_redirects=0$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mpacket redirect (default) is not disabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mpacket redirect (default) is disabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if packet redirect is disabled (default)\t\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.all.send_redirects| grep -cP '^net\.ipv4\.conf\.all\.send_redirects\s=\s0$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mpacket redirect (all) is not disabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mpacket redirect (all) is disabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if packet redirect is disabled for active kernel (all)\t\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.default.send_redirects| grep -cP '^net\.ipv4\.conf\.default\.send_redirects\s=\s0$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mpacket redirect (default) is not disabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mpacket redirect (default) is disabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if packet redirect is disabled for active kernel (default)\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if source routed packets is disabled.."
 signature=$(grep -cP '^net\.ipv4\.conf\.all\.accept_source_route\s=\s0$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msource routed packets (all) is not disabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msource routed packets (all) is disabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if source routed packets is disabled (all)\t\t\t\t\t$status"
 
 signature=$(grep -cP '^net\.ipv4\.conf\.default\.accept_source_route=0$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msource routed packets (default) is not disabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msource routed packets (default) is disabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if source routed packets is disabled (default)\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.all.accept_source_route| grep -cP '^net\.ipv4\.conf\.all\.accept_source_route\s=\s0$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msource routed packets (all) is not disabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msource routed packets (all) is disabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if source routed packets are disabled for active kernel (all)\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.default.accept_source_route| grep -cP '^net\.ipv4\.conf\.default\.accept_source_route\s=\s0$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msource routed packets (default) is not disabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msource routed packets (default) is disabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if source routed packets are disabled for active kernel (default)\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if ICMP redirects is disabled.."
 signature=$(grep -cP '^net\.ipv4\.conf\.all\.accept_redirects\s=\s0$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mICMP redirects (all) is not disabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mICMP redirects (all) is disabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if ICMP redirects are disabled (all)\t\t\t\t\t$status"
 
 signature=$(grep -cP '^net\.ipv4\.conf\.default\.accept_redirects=0$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mICMP redirects (default) is not disabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mICMP redirects (default) is disabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if ICMP redirects are disabled (default)\t\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.all.accept_redirects| grep -cP '^net\.ipv4\.conf\.all\.accept_redirects\s=\s0$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mICMP redirects (all) is not disabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mICMP redirects (all) is disabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if ICMP redirects are disabled for active kernel (all)\t\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.default.accept_redirects| grep -cP '^net\.ipv4\.conf\.default\.accept_redirects\s=\s0$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mICMP redirects (default) is not disabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mICMP redirects (default) is disabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if ICMP redirects are disabled for active kernel (default)\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if suspicious packets logging is enabled.."
 signature=$(grep -cP '^net\.ipv4\.conf\.all\.log_martians\s=\s1$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msuspicious packets logging (all) is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msuspicious packets logging (all) is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if suspicious packets logging is enabled (all)\t\t\t\t$status"
 
 signature=$(grep -cP '^net\.ipv4\.conf\.default\.log_martians=1$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msuspicious packets logging (default) is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msuspicious packets logging (default) is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if suspicious packets logging is enabled (default)\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.all.log_martians| grep -cP '^net\.ipv4\.conf\.all\.log_martians\s=\s1$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msuspicious packets logging (all) is not enabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msuspicious packets logging (all) is enabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if suspicious packets logging is enabled for active kernel (all)\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.default.log_martians| grep -cP '^net\.ipv4\.conf\.default\.log_martians\s=\s1$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91msuspicious packets logging (default) is not enabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92msuspicious packets logging (default) is enabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if suspicious packets logging is enabled for active kernel (default)\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if broadcast ICMP requests are ignored.."
 signature=$(grep -cP '^net\.ipv4\.icmp_echo_ignore_broadcasts=1$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mbroadcast ICMP requests are not ignored..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mbroadcast ICMP requests is ignored."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if broadcast ICMP requests are ignored\t\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.icmp_echo_ignore_broadcasts| grep -cP '^net\.ipv4\.icmp_echo_ignore_broadcasts\s=\s1$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mbroadcast ICMP requests are not ignored on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mbroadcast ICMP requests are ignored on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if broadcast ICMP requests are ignored for active kernel\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if Bad Error Message Protection is enabled.."
 signature=$(grep -cP '^net\.ipv4\.icmp_ignore_bogus_error_responses=1$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mBad Error Message Protection is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mBad Error Message Protection is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Bad Error Message Protection is enabled\t\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.icmp_ignore_bogus_error_responses| grep -cP '^net\.ipv4\.icmp_ignore_bogus_error_responses\s=\s1$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mBad Error Message Protection is not enabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mBad Error Message Protection is enabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Bad Error Message Protection is enabled for active kernel\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if RFC-recommended Source Route Validation is enabled.."
 signature=$(grep -cP '^net\.ipv4\.conf\.all\.rp_filter=1$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mRFC-recommended Source Route Validation (all) is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mRFC-recommended Source Route Validation (all) is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Source Route Validation is enabled (all)\t\t\t\t$status"
 
 signature=$(grep -cP '^net\.ipv4\.conf\.default\.rp_filter=1$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mRFC-recommended Source Route Validation (default) is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mRFC-recommended Source Route Validation (default) is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Source Route Validation is enabled (default)\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.all.rp_filter| grep -cP '^net\.ipv4\.conf\.all\.rp_filter\s=\s1$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mRFC-recommended Source Route Validation (all) is not enabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mRFC-recommended Source Route Validation (all) is enabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Source Route Validation is enabled for active kernel (all)\t\t$status"
 
 signature=$(sysctl net.ipv4.conf.default.rp_filter| grep -cP '^net\.ipv4\.conf\.default\.rp_filter\s=\s1$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mRFC-recommended Source Route Validation (default) is not enabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mRFC-recommended Source Route Validation (default) is enabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Source Route Validation is enabled for active kernel (default)\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if TCP SYN Cookies is enabled.."
 signature=$(grep -cP '^net\.ipv4\.tcp_syncookies=1$' /etc/sysctl.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mTCP SYN Cookies is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mTCP SYN Cookies is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if TCP SYN Cookies is enabled\t\t\t\t\t\t$status"
 
 signature=$(sysctl net.ipv4.tcp_syncookies| grep -cP '^net\.ipv4\.tcp_syncookies\s=\s1$')
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mTCP SYN Cookies is not enabled on active kernel..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mTCP SYN Cookies is enabled on active kernel."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if TCP SYN Cookies is enabled for active kernel\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking TCP Wrappers installation.."
 installed=$(dpkg-query -W -f='${Status}' tcpd 2>/dev/null | grep -c "ok installed")
 if [ $installed -eq 0 ];
 then
-  echo -e "\e[91mtcpd is not installed..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mtcpd is already installed."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking TCP Wrappers installation\t\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking /etc/hosts.allow.."
 fileowner=$(ls -l /etc/hosts.allow| awk '{ print $3 }'|grep -c root)
 if [ $fileowner -eq 0 ];
 then
-  echo -e "\e[91m/etc/hosts.allow is not owned by root..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92m/etc/hosts.allow is owned by root."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking /etc/hosts.allow owner\t\t\t\t\t\t\t$status"
 
 filegroup=$(ls -l /etc/hosts.allow| awk '{ print $4 }'|grep -c root)
 if [ $filegroup -eq 0 ];
 then
-  echo -e "\e[91m/etc/hosts.allow group is not root..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92m/etc/hosts.allow group is root."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking /etc/hosts.allow group\t\t\t\t\t\t\t$status"
 
 fileperms=$(stat --format '%a' /etc/hosts.allow|grep -c 644)
 if [ $fileperms -eq 0 ];
 then
-  echo -e "\e[91m/etc/hosts.allow permissions are not 644..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92m/etc/hosts.allow permissions are 644."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking /etc/hosts.allow file permissions\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking /etc/hosts.deny.."
 fileowner=$(ls -l /etc/hosts.deny| awk '{ print $3 }'|grep -c root)
 if [ $fileowner -eq 0 ];
 then
-  echo -e "\e[91m/etc/hosts.deny is not owned by root..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92m/etc/hosts.deny is owned by root."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking /etc/hosts.deny owner\t\t\t\t\t\t\t$status"
 
 filegroup=$(ls -l /etc/hosts.deny| awk '{ print $4 }'|grep -c root)
 if [ $filegroup -eq 0 ];
 then
-  echo -e "\e[91m/etc/hosts.deny group is not root..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92m/etc/hosts.deny group is root."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking /etc/hosts.deny group\t\t\t\t\t\t\t$status"
 
 fileperms=$(stat --format '%a' /etc/hosts.deny|grep -c 644)
 if [ $fileperms -eq 0 ];
 then
-  echo -e "\e[91m/etc/hosts.deny permissions are not 644..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92m/etc/hosts.deny permissions are 644."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking /etc/hosts.deny file permissions\t\t\t\t\t\t$status"
 
 echo -e "\e[39m"
 
--- audit/nginx.sh
+++ audit/nginx.sh
@@ -24,174 +24,148 @@
 
 
 echo
-echo -e "\e[97m>> nginx audit in progress <<"
-echo
+echo -e "\e[1;95m-------------------------[nginx audit in progress]-------------------------"
 
-echo -e "\e[96m>> Checking nginx installation.."
+
 installed=$(dpkg-query -W -f='${Status}' nginx 2>/dev/null | grep -c "ok installed")
 if [ $installed -eq 0 ];
 then
-  echo -e "\e[91mnginx is not installed..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mnginx is already installed."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking nginx installation\t\t\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if nginx version is hidden.."
 signature=$(grep -cP '\s+server_tokens\soff;$' /etc/nginx/nginx.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mnginx signature is not hidden..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mnginx signature is hidden."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if nginx version is hidden\t\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if ETags is removed.."
 signature=$(grep -cP '^etag\soff;$' /etc/nginx/nginx.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mETags is not removed..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mETags is removed."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if ETags is removed\t\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if index.html is empty.."
 indexmod=$(cat /var/www/html/index.html|wc -w)
 if [ $indexmod -ne 0 ];
 then
-  echo -e "\e[91mindex.html is not empty..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mindex.html is empty."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if index.html is empty\t\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if strong cipher suites are enabled.."
 signature=$(grep -cP '^ssl_ciphers ECDHE-RSA-AES256-GCM-SHA512:DHE-RSA-AES256-GCM-SHA512:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384;$' /etc/nginx/nginx.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mstrong cipher suites are not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mstrong cipher suites are enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if strong cipher suites are enabled\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if ssl session timeout is set.."
 signature=$(grep -cP '^ssl_session_timeout 5m;$' /etc/nginx/nginx.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mssl session timeout is not set..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mssl session timeout is set."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if ssl session timeout is set\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if ssl session cache is set.."
 signature=$(grep -cP '^ssl_session_cache shared:SSL:10m;$' /etc/nginx/nginx.conf)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mssl session cache is not set..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mssl session cache is set."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if ssl session cache is set\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if HttpOnly and Secure flags are enabled.."
 signature=$(grep -cP '^proxy_cookie_path / \"/; secure; HttpOnly\";$' /etc/nginx/sites-available/default)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mHttpOnly and Secure flags are not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mHttpOnly and Secure flags are enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if HttpOnly and Secure flags are enabled\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if Clickjacking Attack Protection is enabled.."
 signature=$(grep -cP '^add_header X-Frame-Options DENY;$' /etc/nginx/sites-available/default)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mClickjacking Attack Protection is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mClickjacking Attack Protection is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Clickjacking Attack Protection is enabled\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if XSS Protection is enabled.."
 signature=$(grep -cP '^add_header X-XSS-Protection \"1; mode=block\";$' /etc/nginx/sites-available/default)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mXSS Protection is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mXSS Protection is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if XSS Protection is enabled\t\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if Enforce secure connections is enabled.."
 signature=$(grep -cP '^add_header Strict-Transport-Security \"max-age=31536000; includeSubdomains;\";$' /etc/nginx/sites-available/default)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mEnforce secure connections is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mEnforce secure connections is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Enforce secure connections is enabled\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if MIME sniffing Protection is enabled.."
 signature=$(grep -cP '^add_header X-Content-Type-Options nosniff;$' /etc/nginx/sites-available/default)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mMIME sniffing Protection is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mMIME sniffing Protection is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if MIME sniffing Protection is enabled\t\t\t\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if Cross-site scripting and injections Protection is enabled.."
 signature=$(grep -cP "^add_header Content-Security-Policy \"default-src 'self';\";$" /etc/nginx/sites-available/default)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mCross-site scripting and injections Protection is not enabled..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mCross-site scripting and injections Protection is enabled."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if Cross-site scripting and injections Protection is enabled\t\t$status"
 
-echo 
-
-echo -e "\e[96m>> Checking if X-Robots-Tag is set.."
 signature=$(grep -cP '^add_header X-Robots-Tag none;$' /etc/nginx/sites-available/default)
 if [ $signature -eq 0 ];
 then
-  echo -e "\e[91mX-Robots-Tag is not set..\e[39m"
+  status="\e[91m[ BAD ]"
   #exit
 else
-  echo -e "\e[92mX-Robots-Tag is set."
+  status="\e[92m[ GOOD ]"
 fi
+echo -e "\e[39m[*] Checking if X-Robots-Tag is set\t\t\t\t\t\t\t$status"
 
 echo -e "\e[39m"
 
