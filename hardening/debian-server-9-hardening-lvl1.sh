51e350d4896c7bd4b1288ec375eb4a81889eaa56
maldevel <maldevel@mail.com>
2019-06-13 16:43:27 +0200

    license

--- hardening/debian-server-9-hardening-lvl1.sh
+++ hardening/debian-server-9-hardening-lvl1.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+#    This file is part of blue-team
+#    Copyright (C) 2019 @maldevel
+#    https://github.com/maldevel/blue-team
+#    
+#    blue-team - Blue Team Scripts.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   
+#    For more see the file 'LICENSE' for copying permission.
+
 #
 # GPLv3
 # Debian 9 new installed default system
