7a1bb985dcee4460fee1b57b46de23826642d875
maldevel <maldevel@mail.com>
2019-06-14 16:53:34 +0200

    debian9 hardening v0.2.1

--- hardening/debian-server-9-hardening-lvl1.sh
+++ hardening/debian-server-9-hardening-lvl1.sh
@@ -28,18 +28,22 @@
 # v0.2.1
 #
 
+
 if [[ "$EUID" -ne 0 ]]; then
   echo "Please run this script as root." 1>&2
   exit 1
 fi
 
+
 # 1.8 Ensure updates, patches, and additional security software are installed
+
 
 apt update
 apt upgrade -y
 
 
 # 6.2.8 Ensure users' home directories permissions are 750 or more restrictive
+
 
 chmod 0750 /var/run/dbus
 chmod 0750 /run/sshd
@@ -50,10 +54,25 @@ chmod 0750 /home/user
 
 # 5.2.15 Ensure only strong Key Exchange algorithms are used
 
+
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-printf '\n%s' 'KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256' >>  /etc/ssh/sshd_config
-printf '\n%s' 'macs umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1' >>  /etc/ssh/sshd_config
-printf '\n%s\n' 'ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com' >>  /etc/ssh/sshd_config
+kexalgorithms=$(grep -cP '^KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256$' /etc/ssh/sshd_config)
+if [ $kexalgorithms -eq 0 ];
+then
+	printf '\n%s' 'KexAlgorithms curve25519-sha256,curve25519-sha256@libssh.org,diffie-hellman-group14-sha256,diffie-hellman-group16-sha512,diffie-hellman-group18-sha512,ecdh-sha2-nistp521,ecdh-sha2-nistp384,ecdh-sha2-nistp256,diffie-hellman-group-exchange-sha256' >>  /etc/ssh/sshd_config
+fi
+
+macs=$(grep -cP '^macs umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1$' /etc/ssh/sshd_config)
+if [ $macs -eq 0 ];
+then
+	printf '\n%s' 'macs umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,hmac-sha1-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1' >>  /etc/ssh/sshd_config
+fi
+
+ciphers=$(grep -cP '^ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com$' /etc/ssh/sshd_config)
+if [ $ciphers -eq 0 ];
+then
+	printf '\n%s\n' 'ciphers chacha20-poly1305@openssh.com,aes128-ctr,aes192-ctr,aes256-ctr,aes128-gcm@openssh.com,aes256-gcm@openssh.com' >>  /etc/ssh/sshd_config
+fi
 systemctl restart ssh
 
 
@@ -73,13 +92,26 @@ chmod o-rwx,g-rw /etc/shadow-
 
 # 5.6 Ensure access to the su command is restricted - /etc/pam.d/su
 
+
 cp /etc/pam.d/su /etc/pam.d/su.bak
-groupadd wheel
+groupwheel=$(getent group wheel 2>/dev/null | grep -c "wheel")
+if [ $groupwheel -eq 0 ];
+then
+	groupadd wheel
+fi
+
 sed -i "s/#.*auth.*required.*pam_wheel\.so$/auth required pam_wheel\.so group=wheel debug/" /etc/pam.d/su
-printf '\n%s\n' '%wheel  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+
+sudowheel=$(grep -cP '^%wheel\s+ALL=\(ALL:ALL\)\s+ALL$' /etc/sudoers)
+if [ $sudowheel -eq 0 ];
+then
+	printf '\n%s\n' '%wheel  ALL=(ALL:ALL) ALL' >> /etc/sudoers
+fi
+
 
 
 # 5.5 Ensure root login is restricted to system console
+
 
 cp /etc/securetty /etc/securetty.bak
 echo > /etc/securetty
@@ -90,8 +122,18 @@ echo > /etc/securetty
 
 cp /etc/bash.bashrc /etc/bash.bashrc.bak
 cp /etc/profile /etc/profile.bak
-printf '\n%s\n' 'umask 027' >> /etc/bash.bashrc
-printf '\n%s\n' 'umask 027' >> /etc/profile
+
+umaskbash=$(grep -cP '^umask 027$' /etc/bash.bashrc)
+if [ $umaskbash -eq 0 ];
+then
+	printf '\n%s\n' 'umask 027' >> /etc/bash.bashrc
+fi
+
+umaskprofile=$(grep -cP '^umask 027$' /etc/profile)
+if [ $umaskprofile -eq 0 ];
+then
+	printf '\n%s\n' 'umask 027' >> /etc/profile
+fi
 
 
 # 5.4.1.4 Ensure inactive password lock is 30 days or less
@@ -105,6 +147,7 @@ for user in `egrep ^[^:]+:[^\!*] /etc/shadow | cut -d: -f1`; do chage --inactive
 # 5.4.1.1 Ensure password expiration is 365 days or less
 # 5.4.1.3 Ensure password expiration warning days is 7 or more
 
+
 cp /etc/login.defs /etc/login.defs.bak
 sed -i "s/^PASS_MIN_DAYS.*/PASS_MIN_DAYS   7/" /etc/login.defs
 sed -i "s/^PASS_MAX_DAYS.*/PASS_MAX_DAYS   90/" /etc/login.defs
@@ -115,28 +158,59 @@ sed -i "s/^PASS_WARN_AGE.*/PASS_WARN_AGE   10/" /etc/login.defs
 
 
 cp /etc/pam.d/common-password /etc/pam.d/common-password.bak
-printf '\n%s\n' 'password required pam_pwhistory.so remember=5' >> /etc/pam.d/common-password
+pampwhistory=$(grep -cP '^password required pam_pwhistory.so remember=5$' /etc/pam.d/common-password)
+if [ $pampwhistory -eq 0 ];
+then
+	printf '\n%s\n' 'password required pam_pwhistory.so remember=5' >> /etc/pam.d/common-password
+fi
 
 
 # 5.3.2 Ensure lockout for failed password attempts is configured
 
 
 cp /etc/pam.d/common-auth /etc/pam.d/common-auth.bak
-printf '\n%s\n' 'auth required pam_tally2.so onerr=fail audit silent deny=5 unlock_time=900' >> /etc/pam.d/common-auth
+pamtally=$(grep -cP '^auth required pam_tally2.so onerr=fail audit silent deny=5 unlock_time=900$' /etc/pam.d/common-auth)
+if [ $pamtally -eq 0 ];
+then
+	printf '\n%s\n' 'auth required pam_tally2.so onerr=fail audit silent deny=5 unlock_time=900' >> /etc/pam.d/common-auth
+fi
 
 
 # 5.3.1 Ensure password creation requirements are configured
 
 
 apt install libpam-pwquality -y
-# cp /etc/pam.d/common-password /etc/pam.d/common-password.bak
-# printf '\n%s\n' 'password requisite pam_pwquality.so retry=3' >> /etc/pam.d/common-password
 cp /etc/security/pwquality.conf /etc/security/pwquality.conf.bak
-printf '\n%s' 'minlen = 14' >> /etc/security/pwquality.conf
-printf '\n%s' 'dcredit = -1' >> /etc/security/pwquality.conf
-printf '\n%s' 'ucredit = -1' >> /etc/security/pwquality.conf
-printf '\n%s' 'ocredit = -1' >> /etc/security/pwquality.conf
-printf '\n%s\n' 'lcredit = -1' >> /etc/security/pwquality.conf
+
+minlen=$(grep -cP '^minlen = 14$' /etc/security/pwquality.conf)
+if [ $minlen -eq 0 ];
+then
+	printf '\n%s' 'minlen = 14' >> /etc/security/pwquality.conf
+fi
+
+dcredit=$(grep -cP '^dcredit = -1$' /etc/security/pwquality.conf)
+if [ $dcredit -eq 0 ];
+then
+	printf '\n%s' 'dcredit = -1' >> /etc/security/pwquality.conf
+fi
+
+ucredit=$(grep -cP '^ucredit = -1$' /etc/security/pwquality.conf)
+if [ $ucredit -eq 0 ];
+then
+	printf '\n%s' 'ucredit = -1' >> /etc/security/pwquality.conf
+fi
+
+ocredit=$(grep -cP '^ocredit = -1$' /etc/security/pwquality.conf)
+if [ $ocredit -eq 0 ];
+then
+	printf '\n%s' 'ocredit = -1' >> /etc/security/pwquality.conf
+fi
+
+lcredit=$(grep -cP '^lcredit = -1$' /etc/security/pwquality.conf)
+if [ $lcredit -eq 0 ];
+then
+	printf '\n%s\n' 'lcredit = -1' >> /etc/security/pwquality.conf
+fi
 
 
 # 5.2.9 Ensure SSH HostbasedAuthentication is disabled
@@ -153,16 +227,29 @@ printf '\n%s\n' 'lcredit = -1' >> /etc/security/pwquality.conf
 # 5.2.10 Ensure SSH root login is disabled
 # 5.2.1 Ensure permissions on /etc/ssh/sshd_config are configured
 
+
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sed -i "s/#Port 22/Port 62111/g" /etc/ssh/sshd_config
 sed -i "s/#HostbasedAuthentication.*no$/HostbasedAuthentication no/" /etc/ssh/sshd_config
 sed -i "s/#IgnoreRhosts.*yes$/IgnoreRhosts yes/" /etc/ssh/sshd_config
 sed -i "s/#MaxAuthTries.*6$/MaxAuthTries 4/" /etc/ssh/sshd_config
 sed -i "s/#LogLevel.*INFO$/LogLevel INFO/" /etc/ssh/sshd_config
-printf '\n%s' 'Protocol 2' >> /etc/ssh/sshd_config
+
+protocol=$(grep -cP '^Protocol 2$' /etc/ssh/sshd_config)
+if [ $protocol -eq 0 ];
+then
+	printf '\n%s' 'Protocol 2' >> /etc/ssh/sshd_config
+fi
+
 cp /etc/issue.net /etc/issue.net.bak
 echo "Welcome" > /etc/issue.net
-printf '\n%s\n' 'Banner /etc/issue.net' >> /etc/ssh/sshd_config
+
+banner=$(grep -cP '^Banner /etc/issue.net$' /etc/ssh/sshd_config)
+if [ $banner -eq 0 ];
+then
+	printf '\n%s\n' 'Banner /etc/issue.net' >> /etc/ssh/sshd_config
+fi
+
 sed -i "s/#LoginGraceTime.*2m$/LoginGraceTime 60/" /etc/ssh/sshd_config
 sed -i "s/#ClientAliveInterval.*0$/ClientAliveInterval 300/" /etc/ssh/sshd_config
 sed -i "s/#ClientAliveCountMax.*3$/ClientAliveCountMax 0/" /etc/ssh/sshd_config
@@ -285,28 +372,42 @@ ip6tables-save > /etc/iptables/rules.v6
 
 
 cp /etc/modprobe.d/tipc.conf /etc/modprobe.d/tipc.conf.bak
-printf '\n%s\n' 'install tipc /bin/true' >> /etc/modprobe.d/tipc.conf
-
+tipc=$(grep -cP '^install tipc /bin/true$' /etc/modprobe.d/tipc.conf)
+if [ $tipc -eq 0 ];
+then
+	printf '\n%s\n' 'install tipc /bin/true' >> /etc/modprobe.d/tipc.conf
+fi
 
 # 3.4.3 Ensure RDS is disabled
 
 
 cp /etc/modprobe.d/rds.conf /etc/modprobe.d/rds.conf.bak
-printf '\n%s\n' 'install rds /bin/true' >> /etc/modprobe.d/rds.conf
-
+rds=$(grep -cP '^install rds /bin/true$' /etc/modprobe.d/rds.conf)
+if [ $rds -eq 0 ];
+then
+	printf '\n%s\n' 'install rds /bin/true' >> /etc/modprobe.d/rds.conf
+fi
 
 # 3.4.2 Ensure SCTP is disabled
 
 
 cp /etc/modprobe.d/sctp.conf /etc/modprobe.d/sctp.conf.bak
-printf '\n%s\n' 'install sctp /bin/true' >> /etc/modprobe.d/sctp.conf
+sctp=$(grep -cP '^install sctp /bin/true$' /etc/modprobe.d/sctp.conf)
+if [ $sctp -eq 0 ];
+then
+	printf '\n%s\n' 'install sctp /bin/true' >> /etc/modprobe.d/sctp.conf
+fi
 
 
 # 3.4.1 Ensure DCCP is disabled
 
 
 cp /etc/modprobe.d/dccp.conf /etc/modprobe.d/dccp.conf.bak
-printf '\n%s\n' 'install dccp /bin/true' >> /etc/modprobe.d/dccp.conf
+dccp=$(grep -cP '^install dccp /bin/true$' /etc/modprobe.d/dccp.conf)
+if [ $dccp -eq 0 ];
+then
+	printf '\n%s\n' 'install dccp /bin/true' >> /etc/modprobe.d/dccp.conf
+fi
 
 
 # 3.2.9 Ensure IPv6 router advertisements are not accepted
@@ -322,8 +423,19 @@ printf '\n%s\n' 'install dccp /bin/true' >> /etc/modprobe.d/dccp.conf
 
 
 cp /etc/sysctl.d/99-sysctl.conf /etc/sysctl.d/99-sysctl.conf.bak
-printf '\n%s\n' 'net.ipv6.conf.all.accept_ra = 0' >> /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv6.conf.default.accept_ra = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv6=$(grep -cP '^net.ipv6.conf.all.accept_ra = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv6 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv6.conf.all.accept_ra = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
+ipv6=$(grep -cP '^net.ipv6.conf.default.accept_ra = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv6 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv6.conf.default.accept_ra = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv6.conf.all.accept_ra=0
 sysctl -w net.ipv6.conf.default.accept_ra=0
 sed -i "s/#net.ipv4.tcp_syncookies=1/net.ipv4.tcp_syncookies=1/g" /etc/sysctl.d/99-sysctl.conf
@@ -332,36 +444,90 @@ sed -i "s/#net.ipv4.conf.all.rp_filter=1/net.ipv4.conf.all.rp_filter=1/g" /etc/s
 sed -i "s/#net.ipv4.conf.default.rp_filter=1/net.ipv4.conf.default.rp_filter=1/g" /etc/sysctl.d/99-sysctl.conf
 sysctl -w net.ipv4.conf.all.rp_filter=1
 sysctl -w net.ipv4.conf.default.rp_filter=1
-printf '\n%s\n' 'net.ipv4.icmp_ignore_bogus_error_responses = 1' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv4=$(grep -cP '^net.ipv4.icmp_ignore_bogus_error_responses = 1$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv4 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv4.icmp_ignore_bogus_error_responses = 1' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv4.icmp_ignore_bogus_error_responses=1
-printf '\n%s\n' 'net.ipv4.icmp_echo_ignore_broadcasts = 1' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv4=$(grep -cP '^net.ipv4.icmp_ignore_bogus_error_responses = 1$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv4 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv4.icmp_echo_ignore_broadcasts = 1' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv4.icmp_echo_ignore_broadcasts=1
 sed -i "s/#net.ipv4.conf.all.log_martians.*=.*1/net.ipv4.conf.all.log_martians = 1/g" /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv4.conf.default.log_martians = 1' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv4=$(grep -cP '^net.ipv4.conf.default.log_martians = 1$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv4 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv4.conf.default.log_martians = 1' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv4.conf.all.log_martians=1
 sysctl -w net.ipv4.conf.default.log_martians=1
 sed -i "s/# net.ipv4.conf.all.secure_redirects.*=.*1/net.ipv4.conf.all.secure_redirects = 0/g" /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv4.conf.default.secure_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv4=$(grep -cP '^net.ipv4.conf.default.secure_redirects = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv4 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv4.conf.default.secure_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv4.conf.all.secure_redirects=0
 sysctl -w net.ipv4.conf.default.secure_redirects=0
 sed -i "s/#net.ipv4.conf.all.accept_redirects.*=.*0/net.ipv4.conf.all.accept_redirects = 0/g" /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv4.conf.default.accept_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv4=$(grep -cP '^net.ipv4.conf.default.accept_redirects = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv4 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv4.conf.default.accept_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sed -i "s/#net.ipv6.conf.all.accept_redirects.*=.*0/net.ipv6.conf.all.accept_redirects = 0/g" /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv6.conf.default.accept_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv6=$(grep -cP '^net.ipv6.conf.default.accept_redirects = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv6 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv6.conf.default.accept_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv4.conf.all.accept_redirects=0
 sysctl -w net.ipv4.conf.default.accept_redirects=0
 sysctl -w net.ipv6.conf.all.accept_redirects=0
 sysctl -w net.ipv6.conf.default.accept_redirects=0
 sed -i "s/#net.ipv4.conf.all.accept_source_route.*=.*0/net.ipv4.conf.all.accept_source_route = 0/g" /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv4.conf.default.accept_source_route = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv4=$(grep -cP '^net.ipv4.conf.default.accept_source_route = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv4 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv4.conf.default.accept_source_route = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sed -i "s/#net.ipv6.conf.all.accept_source_route.*=.*0/net.ipv6.conf.all.accept_source_route = 0/g" /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv6.conf.default.accept_source_route = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv6=$(grep -cP '^net.ipv6.conf.default.accept_source_route = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv6 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv6.conf.default.accept_source_route = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv4.conf.all.accept_source_route=0
 sysctl -w net.ipv4.conf.default.accept_source_route=0
 sysctl -w net.ipv6.conf.all.accept_source_route=0
 sysctl -w net.ipv6.conf.default.accept_source_route=0
 sed -i "s/#net.ipv4.conf.all.send_redirects.*=.*0/net.ipv4.conf.all.send_redirects = 0/g" /etc/sysctl.d/99-sysctl.conf
-printf '\n%s\n' 'net.ipv4.conf.default.send_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+ipv4=$(grep -cP '^net.ipv4.conf.default.send_redirects = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $ipv4 -eq 0 ];
+then
+	printf '\n%s\n' 'net.ipv4.conf.default.send_redirects = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w net.ipv4.conf.all.send_redirects=0
 sysctl -w net.ipv4.conf.default.send_redirects=0
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward = 0/g" /etc/sysctl.d/99-sysctl.conf
@@ -396,8 +562,19 @@ apt-get remove xserver-xorg*
 
 cp /etc/security/limits.conf /etc/security/limits.conf.bak
 cp /etc/sysctl.d/99-sysctl.conf /etc/sysctl.d/99-sysctl.conf.bak2
-printf '\n%s\n' '* hard core 0' >> /etc/security/limits.conf
-printf '\n%s\n' 'fs.suid_dumpable = 0' >> /etc/sysctl.d/99-sysctl.conf
+
+hard=$(grep -cP '^\* hard core 0$' /etc/security/limits.conf)
+if [ $hard -eq 0 ];
+then
+	printf '\n%s\n' '* hard core 0' >> /etc/security/limits.conf
+fi
+
+dumpable=$(grep -cP '^fs.suid_dumpable = 0$' /etc/sysctl.d/99-sysctl.conf)
+if [ $dumpable -eq 0 ];
+then
+	printf '\n%s\n' 'fs.suid_dumpable = 0' >> /etc/sysctl.d/99-sysctl.conf
+fi
+
 sysctl -w fs.suid_dumpable=0
 
 
@@ -412,7 +589,13 @@ chmod og-rwx /boot/grub/grub.cfg
 
 
 cp /etc/modprobe.d/udf.conf /etc/modprobe.d/udf.conf.bak
-printf '\n%s\n' 'install udf /bin/true' >> /etc/modprobe.d/udf.conf
+
+udf=$(grep -cP '^install udf /bin/true$' /etc/modprobe.d/udf.conf)
+if [ $udf -eq 0 ];
+then
+	printf '\n%s\n' 'install udf /bin/true' >> /etc/modprobe.d/udf.conf
+fi
+
 rmmod udf
 
 
@@ -420,7 +603,13 @@ rmmod udf
 
 
 cp /etc/modprobe.d/hfsplus.conf /etc/modprobe.d/hfsplus.conf.bak
-printf '\n%s\n' 'install hfsplus /bin/true' >> /etc/modprobe.d/hfsplus.conf
+
+hfsplus=$(grep -cP '^install hfsplus /bin/true$' /etc/modprobe.d/hfsplus.conf)
+if [ $hfsplus -eq 0 ];
+then
+	printf '\n%s\n' 'install hfsplus /bin/true' >> /etc/modprobe.d/hfsplus.conf
+fi
+
 rmmod hfsplus
 
 
@@ -428,7 +617,13 @@ rmmod hfsplus
 
 
 cp /etc/modprobe.d/hfs.conf /etc/modprobe.d/hfs.conf.bak
-printf '\n%s\n' 'install hfs /bin/true' >> /etc/modprobe.d/hfs.conf
+
+hfs=$(grep -cP '^install hfs /bin/true$' /etc/modprobe.d/hfs.conf)
+if [ $hfs -eq 0 ];
+then
+	printf '\n%s\n' 'install hfs /bin/true' >> /etc/modprobe.d/hfs.conf
+fi
+
 rmmod hfs
 
 
@@ -436,7 +631,13 @@ rmmod hfs
 
 
 cp /etc/modprobe.d/jffs2.conf /etc/modprobe.d/jffs2.conf.bak
-printf '\n%s\n' 'install jffs2 /bin/true' >> /etc/modprobe.d/jffs2.conf
+
+jffs2=$(grep -cP '^install jffs2 /bin/true$' /etc/modprobe.d/jffs2.conf)
+if [ $jffs2 -eq 0 ];
+then
+	printf '\n%s\n' 'install jffs2 /bin/true' >> /etc/modprobe.d/jffs2.conf
+fi
+
 rmmod jffs2
 
 
@@ -444,6 +645,11 @@ rmmod jffs2
 
 
 cp /etc/modprobe.d/freevxfs.conf /etc/modprobe.d/freevxfs.conf.bak
-printf '\n%s\n' 'install freevxfs /bin/true' >> /etc/modprobe.d/freevxfs.conf
-rmmod freevxfs
 
+freevxfs=$(grep -cP '^install freevxfs /bin/true$' /etc/modprobe.d/freevxfs.conf)
+if [ $freevxfs -eq 0 ];
+then
+	printf '\n%s\n' 'install freevxfs /bin/true' >> /etc/modprobe.d/freevxfs.conf
+fi
+
+rmmod freevxfs
